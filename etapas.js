3188df785c2df54d08d4300f0367e164bede7c3d
Guilherme Henrique <ghdesa@gmail.com>
2022-10-25 00:57:04 -0300

    Primeiro Commit

---
+++ etapas.js
@@ -0,0 +1,81 @@
+let etapas = [
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos: [
+        {
+            numero: '13',
+            nome: 'Fernando Haddad',
+            partido: 'PT',
+            fotos: [
+                {url:'assets/images/haddad.png', legenda: 'GOVERNADOR'}
+        ]
+        },
+        {
+            numero: '10',
+            nome: 'Tarcisio Gomes',
+            partido: 'REPUBLICANOS',
+            fotos: [
+                {url: 'assets/images/tarcisio.png', legenda: 'GOVERNADOR'}
+            ]
+        },
+        {
+            numero: '30',
+            nome:'Vinicius Poit',
+            partido: 'NOVO',
+            fotos: [
+                {url: 'assets/images/poit.png', legenda: 'GOVERNADOR'}
+            ]
+        }
+    
+    ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '30',
+                nome: 'Felipe Davila',
+                partido: 'NOVO',
+                vice: 'Tiago',
+                fotos:[
+                    {url:'assets/images/davila.png', legenda: 'PRESIDENTE', small: false},
+                    {url:'assets/images/tiago.png', legenda: 'VICE-PRESIDENTE', small:true}
+                ]
+             },
+             {
+                numero: '13',
+                nome: 'PATATA',
+                partido: 'PT',
+                vice:'Geraldo Alckmin',
+                fotos: [
+                    {url:'assets/images/lula.png', legenda: 'PRESIDENTE', small:false},
+                    {url:'assets/images/alckmin.png', legenda: 'VICE-PRESIDENTE',small: true}
+                ]
+             },
+             {
+                numero: '22',
+                nome:'PATATI',
+                partido: 'PL',
+                vice: 'Braga Netto',
+                fotos: [
+                    {url:'assets/images/bolsonaro.png', legenda: 'PRESIDENTE', small:false},
+                    {url:'assets/images/braga.png', legenda: 'VICE-PRESIDENTE', small: true}
+                ]
+             },
+             {
+                numero: '12',
+                nome:'Ciro Gomes',
+                partido: 'PDT',
+                vice: 'Ana',
+                fotos:[ 
+                    {url:'assets/images/ciro.png', legenda:'PRESIDENTE', small:false},
+                    {url:'assets/images/ana.png', legenda: 'VICE-PRESIDENTE',small:true}
+                ]
+             }
+        ]
+    } 
+
+    
+]
